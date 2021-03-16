01b1dbf7528240457a5fbe8c24b8271e805dc7ec
linxingliang <linxingliang@ihuayue.cn>
2021-03-16 11:01:23 +0800

    update

--- cogdl/data/__init__.py
+++
@@ -1,6 +0,0 @@
-from .data import Data
-from .batch import Batch
-from .dataset import Dataset, MultiGraphDataset
-from .dataloader import DataLoader
-
-__all__ = ["Data", "Batch", "Dataset", "DataLoader", "MultiGraphDataset"]
